6fdf2dccc2d5c1e9642632e1be8fa71ddf341b77
thuancutedethuong <23650471.thuan@student.iuh.edu.vn>
2024-08-12 17:21:21 +0700

    init project

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Đức Thuận");
+        System.out.println("Welcome to the DHKTPM19A!");
+    }
+}
